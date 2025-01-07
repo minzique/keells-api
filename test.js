a4dd482c1360f0f9103a4d2efa5d726d3f229c73
minzi <minzique@protonmail.com>
2025-01-07 19:10:17 +0530

    example impl

---
+++ test.js
@@ -0,0 +1,46 @@
+import { accountApi, authApi, webApi } from "./api.js";
+//loadenv
+import dotenv from 'dotenv';
+import fs from "fs";
+const login = async () => {
+  try {
+    // Login - cookies will be saved automatically
+    dotenv.config({ path: ".env.local" });
+    const { KEELS_USERNAME, KEELS_PASSWORD } = process.env;
+    const response = await authApi.credentialLogin(KEELS_USERNAME, KEELS_PASSWORD);
+    // const response = await authApi.guestLogin();
+    // Verify session after login
+    const session = await authApi.checkSession();
+    console.log('Session status:', session);
+
+    
+    return response;
+
+  } catch (error) {
+    console.error('Login error:', error.message);
+    throw error;
+  }
+}
+
+(async () => {
+  const cus = await login();
+  console.log(cus);
+
+  const shippingDetails = await accountApi.loadShippingDetails();
+  // get the one that includes addressTypeName: primary
+  const primaryShippingDetail = shippingDetails.result.listShippingDetails.find((shippingDetail) => shippingDetail.addressTypeName === 'Primary');
+  const branchCode = shippingDetails.result.suburbs.find(
+    (suburb) => suburb.name === "Colombo 5"
+  ).branchCode;
+  console.log(branchCode);
+
+  const res = await webApi.getInitialData(branchCode, 0);
+  console.log(res.result);
+  // save to json
+
+  fs.writeFileSync('keels.json', JSON.stringify(res.result, null, 2));
+
+  // const items = await webApi.getItemDetails({outletCode: branchCode, departmentId: 16}); 
+  // console.log(items);
+
+} )();
